8da7facd421c8cfcfb0471f26f9548d0004cda3a
poljim <yiluom534@gmail.com>
2026-04-11 17:27:46 +0300

    Fix bugs connected to Adding and Sorting expenses.

--- Expense.cpp
+++ Expense.cpp
@@ -14,7 +14,7 @@ Expense::Expense(int id, const string& name, Category category, double amount, c
     if (date.size() != 10 || date[4]!= '-' || date[7]!='-') {
         throw invalid_argument("Date must be in YYYY-MM-DD format.\n");
     }
-    if (date[5]>2 || date[5]==1 && date[6]>2 || date[8]>3 || date[8]==3 && date[9]>1 || date[1]>0 || date[0]>2 || date[0]<1) {
+    if (date[5]>2 || date[5]==1 && date[6]>2 || date[8]>3 || date[8]==3 && date[9]>1 || date[1]>0 || date[0]>2 || date[0]<1 || date[2]>2 ||date[2]==2 && date[3]>6) {
         throw invalid_argument("Date must be in YYYY-MM-DD format.\n");
     }
     // if (category.empty()) {
@@ -41,7 +41,7 @@ string categoryToString(Category category) {
     }
 }
 
-// The reason they are all constants is getters should not modify values
+// The reason they are all chosen constants is getters should not modify values
 int Expense::getID() const {
     return id;
 }
